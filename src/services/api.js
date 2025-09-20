6b0e3e14eae117688dcc01f96dfe87fb34edeeb8
DAIYAN09248 <2024lcs_mohammeddaiyan_c@nie.ac.in>
2025-09-20 18:16:18 +0530

    project

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://gaming-backend-k7tb.onrender.com/api';
+const API_URL = 'https://gaming-backend-k7tb.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
